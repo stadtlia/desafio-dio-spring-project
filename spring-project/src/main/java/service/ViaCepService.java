abca40929ffe730f0e7c0a4ac45b212f2d224b1d
stadtlia <lidiastadtlober@outlook.com>
2022-07-20 17:48:16 -0300

    First Commit

---
+++ spring-project/src/main/java/service/ViaCepService.java
@@ -0,0 +1,14 @@
+package service;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import one.digital.innovation.springproject.model.Endereco;
+
+@FeignClient(name = "viacep", url = "https://viacep.com.br/ws")
+public interface ViaCepService {
+
+	@GetMapping("/{cep}/json/")
+	Endereco consultarCep(@PathVariable("cep") String cep);
+}
